7aa39ca63674f5b0eb7d2dba0e5dae183bd02837
Justin T. Bricker <jtbricker@infiniteenergy.com>
2018-01-03 01:21:54 -0500

    Add route and mock controller.  Register routes in server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
 
+var routes = require('./api/routes/datacampEmailRoutes');
+routes(app);
+
 app.listen(port);
 
 console.log('email processor RESTful API server started on: ' + port);
