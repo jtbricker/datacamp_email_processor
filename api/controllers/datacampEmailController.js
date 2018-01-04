d8cd3aff641ef88e3400bb358dd4d374c76741b2
Justin Bricker <jt.bricker@gmail.com>
2018-01-03 23:19:34 -0500

    loop over all links

--- api/controllers/datacampEmailController.js
+++ api/controllers/datacampEmailController.js
@@ -3,6 +3,8 @@
 exports.processEmail = function(req, res) {
     const cheerio = require('cheerio');
     const $ = cheerio.load(req.body.body);
-    console.log($('a'));
+    $(links).each(function(i, link){
+        console.log($(link).text() + ':\n  ' + $(link).attr('href'));
+      });
     res.json({ message: 'Message Received' });    
 };
