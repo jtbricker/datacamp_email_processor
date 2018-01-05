8bf30a6ee61aaa1df0939536c21b10b655bfe4ac
Justin Bricker <jt.bricker@gmail.com>
2018-01-04 20:43:41 -0500

    Only log links with text greater than 15 characters in length

--- api/controllers/datacampEmailController.js
+++ api/controllers/datacampEmailController.js
@@ -3,9 +3,14 @@
 exports.processEmail = function(req, res) {
     const cheerio = require('cheerio');
     const $ = cheerio.load(req.body.body);
+    const date = er
     var links = $('a');
     $(links).each(function(i, link){
-        console.log($(link).text() + ':\n  ' + $(link).attr('href'));
+        link_text = $(link).text();
+        link_url = $(link).attr('href');
+        if(link_text.length > 15){
+            console.log( link_text + ':\n  ' + link_url);
+        }
       });
     res.json({ message: 'Message Received' });    
 };
